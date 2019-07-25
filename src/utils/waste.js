171934097387990a2d77fbc4010d3e24f2ad5a6b
xiaodong <jokimina@163.com>
2019-07-25 11:27:07 +0800

    add: more waste type

--- src/utils/waste.js
+++ src/utils/waste.js
@@ -1,4 +1,4 @@
-export const wasteArr = ['有害垃圾', '可回收物', '湿垃圾', '干垃圾'];
+export const wasteArr = ['有害垃圾', '可回收物', '湿垃圾', '干垃圾', '不属于垃圾, 可能有害', '不属于垃圾, 可以回收', '不属于日常生活垃圾', '装修垃圾', '大件垃圾'];
 
 export const getWasteNameByIndex = i => wasteArr[i];
 export const getWasteIndexByName = v => wasteArr[wasteArr.indexOf(v)];
